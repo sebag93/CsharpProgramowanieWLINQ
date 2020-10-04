b7b827242253995ea345a391baaba66c04cb8017
Sebastian Guzdek <sebag93@gmail.com>
2020-10-04 21:40:09 +0200

    4.1 Wprowadzenie

---
+++ LINQ/Samochody/Samochod.cs
@@ -0,0 +1,14 @@
+﻿namespace Samochody
+{
+    public class Samochod
+    {
+        public int Rok { get; set; }
+        public string Producent { get; set; }
+        public string Model { get; set; }
+        public double Pojemnosc { get; set; }
+        public int IloscCylindrow { get; set; }
+        public int SpalanieMiasto { get; set; }
+        public int SpalanieAutostrada { get; set; }
+        public int SpalanieMieszane { get; set; }
+    }
+}
