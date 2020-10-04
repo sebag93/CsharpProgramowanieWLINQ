662af5c7c8ee1698171700175f26fee5e78c7a1c
Sebastian Guzdek <sebag93@gmail.com>
2020-10-04 22:45:27 +0200

    4.4 Implementacja wczytywania pliku - składnia zapytania

--- LINQ/Samochody/Program.cs
+++ LINQ/Samochody/Program.cs
@@ -9,12 +9,20 @@ namespace Samochody
     {
         static void Main(string[] args)
         {
-            var samochody = WczytywaniePliku("C:/KURSY/.NetDev/4/CsharpProgramowanieWLINQ/LINQ/Samochody/paliwo.csv");
+            var samochody = WczytywaniePliku2("paliwo.csv");
 
             foreach (var samochod in samochody)
             {
                 Console.WriteLine(samochod.Producent + " " + samochod.Model);
             }
+        }
+
+        private static List<Samochod> WczytywaniePliku2(string sciezka)
+        {
+            var zapytanie = from linia in File.ReadAllLines(sciezka).Skip(1)
+                            where linia.Length > 1
+                            select Samochod.ParsujCSV(linia);
+            return zapytanie.ToList();
         }
 
         private static List<Samochod> WczytywaniePliku(string sciezka)
