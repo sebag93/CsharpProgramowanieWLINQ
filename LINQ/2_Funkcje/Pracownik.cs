b14becf20b638dda9306fa61fb984c48ea047452
Sebastian Guzdek <sebag93@gmail.com>
2020-10-04 12:16:45 +0200

    2.2 Siła IEnumerable

---
+++ LINQ/2_Funkcje/Pracownik.cs
@@ -0,0 +1,9 @@
+﻿namespace _2_Funkcje
+{
+    public class Pracownik
+    {
+        public int Id { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+    }
+}
---
+++ LINQ/2_Funkcje/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Funkcje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Pracownik> programisci = new Pracownik[]
+            {
+                new Pracownik { Id = 1, Imie = "Marcin", Nazwisko = "Nowak"},
+                new Pracownik { Id = 2, Imie = "Tomek", Nazwisko = "Kowal"},
+                new Pracownik { Id = 3, Imie = "Jacek", Nazwisko = "Sobota"},
+                new Pracownik { Id = 4, Imie = "Adam", Nazwisko = "Wrona"}
+            };
+
+            IEnumerable<Pracownik> kierowcy = new List<Pracownik>()
+            {
+                new Pracownik { Id = 5, Imie = "Olek", Nazwisko = "Sroka"},
+                new Pracownik { Id = 6, Imie = "Pawel", Nazwisko = "Wrobel"},
+                new Pracownik { Id = 7, Imie = "Marek", Nazwisko = "Piatek"}
+            };
+
+            IEnumerator<Pracownik> enumerator = kierowcy.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Imie); 
+            }
+        }
+    }
+}
